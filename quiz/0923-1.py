321b2d20065cfc59eb09b09e47f39ad7e2f9cbb1
zhanallen <130176404+zhanallen@users.noreply.github.com>
2023-09-27 01:29:09 +0800

    Add files via upload
    quiz

---
+++ quiz/0923-1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 09:08:38 2023
+
+@author: allen
+"""
+
+x = input()
+print("[Output] Dear ", x, ", ...", sep = "")
---
+++ quiz/0923-2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 09:08:38 2023
+
+@author: allen
+"""
+
+x = input()
+y = input()
+print("[Output] Dear ", x, " ", y, ", ...", sep = "")
