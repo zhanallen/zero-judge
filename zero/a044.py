c41b3eae32ad0971e211a5a66880f0100ae98d4a
zhanallen <130176404+zhanallen@users.noreply.github.com>
2023-09-28 02:55:14 +0800

    Add files via upload
    a044 upload

---
+++ zero/a044.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 22:58:36 2023
+
+@author: allen
+
+感謝提供公式解法
+"""
+
+while (1):
+    try:
+        x = int(input())
+        a = int(((x ** 3) + (5 * x) + 6) / 6)
+        print(a)
+    except:
+        break
