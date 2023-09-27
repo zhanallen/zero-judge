ce0b63c4cf0ffb1e7f110ba10e90425ce8ba12cf
zhanallen <130176404+zhanallen@users.noreply.github.com>
2023-09-27 21:30:20 +0800

    Add files via upload
    a017 updata

---
+++ zero/a017.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 21:24:18 2023
+
+@author: allen
+"""
+
+while (1):
+    try:
+        x = eval(input().replace("/", "//"))
+        print(x)
+        
+    except:
+        break
