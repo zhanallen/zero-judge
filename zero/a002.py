8166602600b8426ee33d3e20a695595f9d1a81b8
zhanallen <allen940403allen@gmail.com>
2023-09-30 01:37:03 +0800

    add annotation

--- zero/a002.py
+++ zero/a002.py
@@ -5,5 +5,5 @@ Created on Tue Sep 26 21:51:19 2023
 @author: allen
 """
 
-x, y = map(int, input().split())  # 先使用spilt把樹入的值切割，再使用map()涵式把輸入的兩個值寫入x, y裡面
+x, y = map(int, input().split())  # 先使用spilt()把輸入的值切割，再使用map()涵式把輸入的兩個值寫入x, y裡面
 print(x + y)
