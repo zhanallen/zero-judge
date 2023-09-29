ba35d54a6959de345a29ff2ceb28963bd8b69d3a
zhanallen <allen940403allen@gmail.com>
2023-09-30 01:50:20 +0800

    Update a004.py

--- zero/a004.py
+++ zero/a004.py
@@ -5,13 +5,13 @@ Created on Thu Sep 14 20:56:04 2023
 @author: allen
 """
 
-while(1):
-    try:
+while (1):                # 使用while製作一個無窮迴圈
+    try:                  # 如果try裡面的程式沒有錯誤則會一直執行，否則將會執行except
         x = int(input())
         if((x % 4 == 0 and x % 100 != 0) or x % 400 == 0):
             print("閏年")
         else:
             print("平年")
-    except:
+    except:               # 進入except後執行break終止while迴圈
         break
     
