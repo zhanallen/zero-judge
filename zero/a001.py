b61a17288042c1a436a647fbc2e45cfe7d7dc234
zhanallen <allen940403allen@gmail.com>
2023-09-30 01:32:19 +0800

    add annotation

--- zero/a001.py
+++ zero/a001.py
@@ -6,4 +6,4 @@ Created on Tue Sep 26 21:45:24 2023
 """
 
 x = input()
-print("hello," , x)
+print("hello," , x) # x前面使用的是 , 所以輸出時預設會有一個空白
--- zero/a002.py
+++ zero/a002.py
@@ -5,5 +5,5 @@ Created on Tue Sep 26 21:51:19 2023
 @author: allen
 """
 
-x, y = map(int, input().split())
+x, y = map(int, input().split())  # 使用map()涵式把輸入的兩個值寫入x, y裡面
 print(x + y)
