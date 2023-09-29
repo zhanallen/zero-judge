d7dd76f8ed4d289577f802eb90a21079df8a164e
zhanallen <allen940403allen@gmail.com>
2023-09-30 03:00:40 +0800

    Update a009.py

--- zero/a009.py
+++ zero/a009.py
@@ -6,5 +6,5 @@ Created on Fri Sep 15 01:54:12 2023
 """
 x = input()
 for i in range(len(x)):
-    a = ord(x[i])    #儲存a為x[i]的ASCII碼
+    a = ord(x[i])    # 儲存a為x[i]的ASCII碼
     print(chr(a - 7), end = "")
