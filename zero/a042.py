352c9d54619cd6b60dc8fba15a797187145f2d81
zhanallen <allen940403allen@gmail.com>
2023-09-30 03:21:57 +0800

    Update a042.py

--- zero/a042.py
+++ zero/a042.py
@@ -3,11 +3,12 @@
 Created on Wed Sep 27 22:30:51 2023
 
 @author: allen
+
 """
 
 while (1):
     try:
         x = int(input())
-        print(int(2 * (((x * (x - 1)) / 2) + 1)))
+        print(int(2 * (((x * (x - 1)) / 2) + 1)))    #算出公式直接套用
     except:
         break
