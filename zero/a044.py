f7196e253ee8e8d56d30b3961a2faad0b3f88010
zhanallen <allen940403allen@gmail.com>
2023-09-30 02:04:42 +0800

    Update a044.py

--- zero/a044.py
+++ zero/a044.py
@@ -4,7 +4,10 @@ Created on Wed Sep 27 22:58:36 2023
 
 @author: allen
 
-感謝提供公式解法
+感謝陪我熬夜解公式的各位同學
+公式解法請參考
+https://wenku.baidu.com/view/61e116ecd6bbfd0a79563c1ec5da50e2524dd106?pcf=2&_wkts_=1696010608632&needWelcomeRecommand=1&bfetype=new&bfetype=new
+
 """
 
 while (1):
