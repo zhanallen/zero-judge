de1bcaa992b90b6aafb3053e54816f8026949f17
zhanallen <allen940403allen@gmail.com>
2023-09-30 01:45:33 +0800

    Update a004.py

--- zero/a004.py
+++ zero/a004.py
@@ -4,13 +4,14 @@ Created on Thu Sep 14 20:56:04 2023
 
 @author: allen
 """
-try:
-    while(1):
+
+while(1):
+    try:
         x = int(input())
         if((x % 4 == 0 and x % 100 != 0) or x % 400 == 0):
             print("閏年")
         else:
             print("平年")
-except:
-    print("", end = "\n")
+    except:
+        break
     
