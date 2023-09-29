992c18e99fc4efc00825479f8007b5d01b103353
zhanallen <allen940403allen@gmail.com>
2023-09-30 03:14:02 +0800

    Update a017.py

--- zero/a017.py
+++ zero/a017.py
@@ -7,8 +7,7 @@ Created on Wed Sep 27 21:24:18 2023
 
 while (1):
     try:
-        x = eval(input().replace("/", "//"))
+        x = eval(input().replace("/", "//"))    # 本題題目要求整除，所以使用者如果輸入/
         print(x)
-        
     except:
         break
