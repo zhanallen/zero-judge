24f1c72c5f7953637df87096e93ed0def03cac57
zhanallen <allen940403allen@gmail.com>
2023-09-30 01:54:58 +0800

    Update a005.py

--- zero/a005.py
+++ zero/a005.py
@@ -4,10 +4,10 @@ Created on Thu Sep 14 22:21:02 2023
 
 @author: allen
 """
-x = int(input())
+x = int(input())    # 先輸入要執行的次數
 for i in range(x):
-    a = input().split()
-    if (int(a[1]) - int(a[0]) == (int(a[2]) - int(a[1]))):
+    a = input().split()    # 切割輸入的字串
+    if (int(a[1]) - int(a[0]) == (int(a[2]) - int(a[1]))):    # 判斷為等差還是等比數列並輸出
         for c in range(len(a)):
             print(a[c], end = " ")
         print(int(a[3]) + (int(a[1]) - int(a[0])))
