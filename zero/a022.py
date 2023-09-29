df5fbdc5ffa34452d96370e749706be186a5e39e
zhanallen <allen940403allen@gmail.com>
2023-09-30 03:17:47 +0800

    Update a022.py

--- zero/a022.py
+++ zero/a022.py
@@ -8,7 +8,7 @@ Created on Wed Sep 27 21:31:23 2023
 import math
 
 x = input()
-l = math.ceil(len(x) / 2)
+l = math.ceil(len(x) / 2)    # 使用字串長度除2並無條件進位找出中間值
 a = x[0:l:1]
 b = x[-1:(-l - 1):-1]
 if (a == b):
