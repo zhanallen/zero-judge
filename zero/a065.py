8795aabba56460f2940c2f647d547acf2e6fc488
zhanallen <allen940403allen@gmail.com>
2023-10-01 03:49:34 +0800

    Create a065.py

---
+++ zero/a065.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  1 03:41:52 2023
+
+@author: allen
+"""
+
+x = input()
+for i in range(len(x) - 1):
+    a = abs(ord(x[i]) - ord(x[i + 1]))
+    print(a,end = "")
