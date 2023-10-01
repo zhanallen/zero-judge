436e42cf827c195d973ce9a95e0f737410a5f0b8
zhanallen <allen940403allen@gmail.com>
2023-10-02 01:14:27 +0800

    Update 2.6.11.3. Ex. 3.py

--- 講義練習題/2.6.11.3. Ex. 3.py
+++ 講義練習題/2.6.11.3. Ex. 3.py
@@ -5,8 +5,8 @@ Created on Mon Oct  2 00:47:01 2023
 @author: allen
 """
 
-x = int(input())
-y = int(input())
+x = int(input("輸入存款金額 :"))
+y = int(input("輸入存入時間(年) :"))
 for _ in range(y * 2):
     x *= 1.005
 print(int(x))
