c0e2b25bcf6460c21f0488d28b12b3c86cda8980
zhanallen <allen940403allen@gmail.com>
2023-10-01 23:26:32 +0800

    Update 2.8.4.5. Ex. 5.py

--- 講義練習題/2.8.4.5. Ex. 5.py
+++ 講義練習題/2.8.4.5. Ex. 5.py
@@ -5,10 +5,10 @@ Created on Sun Oct  1 21:58:52 2023
 @author: allen
 """
 
-x = input().replace("」", "").replace("「", "").split("、")
-if (len(x[1]) < 2):
+x = input().replace("」", "").replace("「", "").split("、")    # 先把上下括號去掉，使用頓號進行切割
+if (len(x[1]) < 2):    # 如果月跟日只有個位數就在前面加0
     x[1] = "0"+x[1]
 if (len(x[2]) < 2):
     x[2] = "0"+x[2]
-a = ".".join(x)
+a = ".".join(x)    # 把x串列用.合併成一個字串
 print(a)
