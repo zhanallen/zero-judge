8a0cc232e28b51dbb7159bb481405c29b0b3286f
zhanallen <allen940403allen@gmail.com>
2023-10-02 02:00:11 +0800

    Create 2.6.11.10. Ex. 10.py

---
+++ 講義練習題/2.6.11.10. Ex. 10.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 01:51:34 2023
+
+@author: allen
+
+預設變數a = True且b = False，
+請寫一個程式輸出a xor b的結果。
+xor為互斥運算，
+當兩個運算元值不同時輸出true，
+其餘則輸出false。
+
+"""
+
+a = True; b = False
+print(a ^ b)
