8f54c8b59b76bbc27529683f6efc37760e6e44b1
zhanallen <allen940403allen@gmail.com>
2023-10-01 13:07:29 +0800

    Update a012.py

--- zero/a012.py
+++ zero/a012.py
@@ -7,7 +7,7 @@ Created on Sun Oct  1 13:01:51 2023
 
 while (1):
     try:
-        a, b = map(int,input().split())
+        a, b = map(int, input().split())
         print(abs(a - b))
     except:
         break
