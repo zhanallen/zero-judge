854d588bc0d12a41b8ae4ccb1bc46ed8e7afc0ba
zhanallen <allen940403allen@gmail.com>
2023-10-02 09:11:03 +0800

    Add quiz

---
+++ quiz/1002-2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 09:04:07 2023
+
+@author: allen
+
+據輸入內容，輸出特定的運算結果。
+
+輸入說明
+輸入某兩個字串
+
+輸出說明
+輸出第一個字串第三個字元之後與第二個字串倒數第三個之前的合併結果
+
+範例輸入 #1
+debug
+coders
+範例輸出 #1
+bug code
+範例輸入 #2
+uncertain
+things
+範例輸出 #2
+certain thin
+
+"""
+
+x = input()
+y = input()
+print(x[2:],y[:-2])
