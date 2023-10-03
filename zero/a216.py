64eaa5f91c607bc46a023a5dc749b56a3a772c1d
zhanallen <allen940403allen@gmail.com>
2023-10-03 10:23:38 +0800

    Update a216.py

--- zero/a216.py
+++ zero/a216.py
@@ -8,7 +8,7 @@ Created on Tue Oct  3 09:55:52 2023
 def f(n):
         return int(((n + 1) * n) / 2)
 def g(n):
-    return int((n * (n+1)*(n+2)) / 6)
+    return int((n * (n+1) * (n+2)) / 6)
 
 while (1):
     try:
