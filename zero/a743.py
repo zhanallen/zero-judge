7fd148da5ea93ebbf87105e609587b937adde051
zhanallen <allen940403allen@gmail.com>
2023-10-14 23:08:55 +0800

    Create a743.py

---
+++ zero/a743.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 14 22:54:12 2023
+
+@author: allen
+"""
+
+x = int(input())
+c = list()
+n = list()
+for i in range(x):
+   a = list(map(str, input().split()))
+   c.append(a[0])
+   if ((a[0] in n) != True):
+       n.append(a[0])
+n.sort()
+for i in n:
+    print(i,c.count(i))
